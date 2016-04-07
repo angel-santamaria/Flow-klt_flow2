460fa9de8b84f615fc6c96575cb77d34e96765f2
asantamaria <asantamaria@iri.upc.edu>
2016-04-07 18:01:04 -0400

    minor change

--- src/settings.c
+++ src/settings.c
@@ -78,8 +78,8 @@ void global_data_reset_param_defaults(void){
 	strcpy(global_data.param_name[PARAM_USART2_BAUD], "USART_2_BAUD");
 	global_data.param_access[PARAM_USART2_BAUD] = READ_ONLY;
 
-	// global_data.param[PARAM_USART3_BAUD] = 115200;
-	global_data.param[PARAM_USART3_BAUD] = 921600;
+	global_data.param[PARAM_USART3_BAUD] = 115200;
+	// global_data.param[PARAM_USART3_BAUD] = 921600;
 	strcpy(global_data.param_name[PARAM_USART3_BAUD], "USART_3_BAUD");
 	global_data.param_access[PARAM_USART3_BAUD] = READ_ONLY;
 
@@ -170,8 +170,8 @@ void global_data_reset_param_defaults(void){
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_FEATURE_THRESHOLD], "BFLOW_F_THLD");
 	global_data.param_access[PARAM_BOTTOM_FLOW_FEATURE_THRESHOLD] = READ_WRITE;
 
-	// global_data.param[PARAM_BOTTOM_FLOW_HIST_FILTER] = 0;
-	global_data.param[PARAM_BOTTOM_FLOW_HIST_FILTER] = 1;
+	global_data.param[PARAM_BOTTOM_FLOW_HIST_FILTER] = 0;
+	// global_data.param[PARAM_BOTTOM_FLOW_HIST_FILTER] = 1;
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_HIST_FILTER], "BFLOW_HIST_FIL");
 	global_data.param_access[PARAM_BOTTOM_FLOW_HIST_FILTER] = READ_WRITE;
 
@@ -180,8 +180,8 @@ void global_data_reset_param_defaults(void){
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION], "BFLOW_GYRO_COM");
 	global_data.param_access[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = READ_WRITE;
 
-	// global_data.param[PARAM_BOTTOM_FLOW_LP_FILTERED] = 0;
-	global_data.param[PARAM_BOTTOM_FLOW_LP_FILTERED] = 1;
+	global_data.param[PARAM_BOTTOM_FLOW_LP_FILTERED] = 0;
+	// global_data.param[PARAM_BOTTOM_FLOW_LP_FILTERED] = 1;
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_LP_FILTERED], "BFLOW_LP_FIL");
 	global_data.param_access[PARAM_BOTTOM_FLOW_LP_FILTERED] = READ_WRITE;
 
